80973fcae5c00b6d7b787c8cad57b31c8d2b6b8d
Karina <Karina@10.0.0.31>
2024-01-08 22:10:47 -0500

    formating code

--- src/db/DbTest.java
+++ src/db/DbTest.java
@@ -3,14 +3,14 @@ package db;
 public class DbTest {
 
 	public static void main(String[] args) {
-		
+
 	}
-	
+
 	public void add() {
-		System.out.println( 1 + 2);
+		System.out.println(1 + 2);
 	}
-	
+
 	public static void subtract(int a, int b) {
-		System.out.println(a-b);
+		System.out.println(a - b);
 	}
 }
