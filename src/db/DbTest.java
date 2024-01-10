e3af5e8f2f1f273cc738bd5b1a6da3f97b83a583
Karina <Karina@10.0.0.31>
2024-01-10 18:16:32 -0500

    divide & multiply ( )

--- src/db/DbTest.java
+++ src/db/DbTest.java
@@ -3,8 +3,8 @@ package db;
 public class DbTest {
 
 	public static void main(String[] args) {
-	
-	System.out.println("Arithmetic Operations");	
+
+		System.out.println("Arithmetic Operations");
 
 	}
 
@@ -15,4 +15,13 @@ public class DbTest {
 	public static void subtract(int a, int b) {
 		System.out.println(a - b);
 	}
+
+	public static void multiply(int a, int b) {
+		System.out.println(a * b);
+	}
+
+	public static void divide(int a, int b) {
+		System.out.println(a / b);
+	}
+
 }
